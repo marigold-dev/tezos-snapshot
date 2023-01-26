3dc94fda77f9f6b548b666ae10cc7c3d4f950b28
Felipe Cardozo <fraifelipe@gmail.com>
2023-01-26 00:02:24 -0300

    add loading bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,8 @@ import React, { CSSProperties, useEffect, useState } from 'react'
 import axios from 'axios'
 import { useTheme, ThemeProvider } from '@mui/material/styles'
 import { Snapshot } from './models/Snapshot'
-import SnapshotItem from './components/SnapshotItem'
 import HeaderBar from './components/HeaderBar'
-import DateSeparator from './components/DateSeparator'
+import Content from './components/Content'
 
 export function App() {
   const theme = useTheme()
@@ -24,12 +23,6 @@ export function App() {
       overflow: 'auto',
       height: 'calc(100vh - 66px)',
     } as CSSProperties,
-    content: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      fontSize: 'calc(2vmin)',
-    } as CSSProperties,
   }
 
   return (
@@ -37,20 +30,7 @@ export function App() {
       <HeaderBar></HeaderBar>
 
       <div style={styles.page}>
-        <div style={styles.content}>
-          {snapshots.map((snapshot, index, array) => (
-            <div
-              key={snapshot.PublicURL}
-              style={{ paddingBottom: '30px', textAlign: 'left' }}
-            >
-              {hasNotPreviousDate(index, array, snapshot) && (
-                <DateSeparator snapshot={snapshot}></DateSeparator>
-              )}
-
-              <SnapshotItem snapshot={snapshot}></SnapshotItem>
-            </div>
-          ))}
-        </div>
+        <Content />
       </div>
     </ThemeProvider>
   )
---
+++ frontend/src/components/Content.tsx
@@ -0,0 +1,67 @@
+import React, { CSSProperties, useEffect, useState } from 'react'
+import axios from 'axios'
+import { useTheme } from '@mui/material/styles'
+import { Snapshot } from '../models/Snapshot'
+import DateSeparator from './DateSeparator'
+import SnapshotItem from './SnapshotItem'
+import ReactLoading from 'react-loading'
+
+const hasNotPreviousDate = (
+  index: number,
+  array: Snapshot[],
+  snapshot: Snapshot
+) => (index > 0 && !(array[index - 1].Date === snapshot.Date)) || index === 0
+
+export default function Content() {
+  const theme = useTheme()
+
+  const [loading, setLoading] = useState(true)
+  const [snapshots, setSnapshots] = useState<Array<Snapshot>>([])
+  useEffect(() => {
+    if (snapshots.length < 1) {
+      axios.get(process.env.REACT_APP_BACKEND_URL!).then((request: any) => {
+        setSnapshots(request.data)
+        setLoading(false)
+      })
+    }
+  }, [snapshots, setSnapshots])
+
+  const styles = {
+    content: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      fontSize: 'calc(2vmin)',
+    } as CSSProperties,
+    loading: {
+      justifyContent: 'center',
+      display: 'flex',
+      height: 'calc(100vh - 66px)',
+      alignItems: 'center',
+    },
+  }
+
+  if (loading)
+    return (
+      <div style={styles.loading}>
+        <ReactLoading type="spin" color={theme.palette.secondary.main} />
+      </div>
+    )
+
+  return (
+    <div style={styles.content}>
+      {snapshots.map((snapshot, index, array) => (
+        <div
+          key={snapshot.PublicURL}
+          style={{ paddingBottom: '30px', textAlign: 'left' }}
+        >
+          {hasNotPreviousDate(index, array, snapshot) && (
+            <DateSeparator snapshot={snapshot}></DateSeparator>
+          )}
+
+          <SnapshotItem snapshot={snapshot}></SnapshotItem>
+        </div>
+      ))}
+    </div>
+  )
+}
